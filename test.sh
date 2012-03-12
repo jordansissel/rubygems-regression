2b889f6dba07d2848a5176566a9cf652a63acfa6
Jordan Sissel <jls@semicomplete.com>
2012-03-11 22:34:47 -0700

    .

--- test.sh
+++ test.sh
@@ -17,55 +17,10 @@ if [ ! -f $rvmloader ] ; then
 fi
 
 . $rvmloader
+. $(dirname $0)/test.subr
 
-use() {
-  rubyver=$1
-  gemver=$2
-
-  rvm use $rubyver
-  rvm gemset create $(gemset)
-  rvm gemset use $(gemset)
-  rvm --force gemset empty $(gemset)
-  rvm install rubygems $gemver
-}
-
-gemset() {
-  echo "temp-$$"
-}
-
-build() {
-  dir=$(dirname $1)
-  gemspec=$(basename $1)
-  find $dir -maxdepth 1 -name '*.gem' -delete
-  (cd $dir; gem build $gemspec)
-}
-
-clean() {
-  rvm --force gemset delete $(gemset)
-}
-
-
+# Prep
 (cd fixtures; sh fpm.sh)
 
-use 1.9.3 1.8.17
-build fixtures/fpm/fpm.gemspec
-clean
-
-# simulate Ubuntu 11.10's defaults
-use 1.8.7 1.7.2
-
-# Test it
-gem install --ignore-dependencies fixtures/fpm/fpm*.gem
-ruby -rubygems -e 'gem "fpm"' 
-result=$?
-
-clean
-
-# Make sure it installed.
-if [ $? -ne 0 ] ; then
-  echo "FAILED"
-else
-  echo "OK"
-fi
-
+check fpm 1.8.7 1.7.2
 
